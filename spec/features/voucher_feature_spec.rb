8b334ff74d05fcd28fccb084c9fb9eb0ddfd0914
marcoaam <marco.army@gmail.com>
2014-09-27 13:17:57 +0100

    voucher  message improved

--- spec/features/voucher_feature_spec.rb
+++ spec/features/voucher_feature_spec.rb
@@ -20,7 +20,7 @@ feature 'Voucher' do
         fill_in :voucher, with: 'GET5off'
         click_button 'Apply voucher'
       end
-      expect(page).to have_content 'Discount: - £ 5'
+      expect(page).to have_content 'Voucher: £ -5'
       expect(page).to have_content 'Total price: £ 94.0'
     end
 
@@ -31,7 +31,7 @@ feature 'Voucher' do
         fill_in :voucher, with: 'GET10off'
         click_button 'Apply voucher'
       end
-      expect(page).to have_content 'Discount: - £ 10'
+      expect(page).to have_content 'Voucher: £ -10'
       expect(page).to have_content 'Total price: £ 89.0'
     end
 
@@ -42,7 +42,7 @@ feature 'Voucher' do
         fill_in :voucher, with: 'GET15off'
         click_button 'Apply voucher'
       end
-      expect(page).to have_content 'Discount: - £ 15'
+      expect(page).to have_content 'Voucher: £ -15'
       expect(page).to have_content 'Total price: £ 84.0'
     end
 
